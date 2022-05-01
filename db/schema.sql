2f97f8a013b99d8d5441c04c1c666a83fcd424f8
sharkbyte <sgquin@gmail.com>
2022-05-01 12:10:02 -0700

    database schema completed

--- db/schema.sql
+++ db/schema.sql
@@ -5,5 +5,25 @@ USE employeeHR_db;
 
 DROP TABLE IF EXISTS departments;
 CREATE TABLE departments (
-  attribute(s)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  dept_name VARCHAR(30) NOT NULL 
+);
+
+DROP TABLE IF EXISTS role;
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  FOREIGN KEY(department_id) REFERENCES departments(id) ON DELETE SET NULL
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE SET NULL,
+  manager_id INT REFERENCES employee(id)
 );
